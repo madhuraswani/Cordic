6de6c9a9a0633dc3b34a2d0bf56d7dfd7e488b7d
Madhur Aswani <madhuraswani06@gmail.com>
2023-10-25 11:16:19 +0530

    Update cordic_tb copy.v

--- cordic_tb copy.v
+++ cordic_tb copy.v
@@ -37,21 +37,22 @@ clk=~clk;
 end
 
 initial begin
-    $monitor("Cycle: %t \t Cosine: %f \t Sine: %f \t angle: %f \t actual_sine: %f \t actual_cosine: %f \n \t sine_error= %f \t cos_error= %f",$time/period,$itor(cosine*o_SF),$itor(sine*o_SF),$itor(in*sf),$sin($itor(in*sf)),$cos($itor(in*sf)),($sin($itor(in*sf))-$itor(sine*o_SF)),($cos($itor(in*sf))-$itor(cosine*o_SF)));
+    $monitor("Cycle: %t \t Cosine: %f \t Sine: %f \t angle: %f \t actual_sine: %f \t actual_cosine: %f \n",$time/period,$itor(cosine*o_SF),$itor(sine*o_SF),$itor(in*sf),$sin($itor(in*sf)),$cos($itor(in*sf)));
     #5 rst_n = 1'b0;
 	#5 rst_n = 1'b1;
     in=8'b00_001110;
     repeat(1) @(posedge clk);
     //in=8'b00_101011;
     //in=8'b00_101100;
-    //in=8'b10_011101;
+    in=8'b10_011101;
+    repeat(1) @(posedge clk);
     in=8'b01_100011; //near pi/2
     repeat(1) @(posedge clk);
     in=8'b00_000000;
     repeat(1) @(posedge clk);
-    in=8'b00_110010;
+    in=8'b00_110010; // near pi/4
     repeat(1) @(posedge clk);
-    in=8'b11_001110; //near -pi/2
+    in=8'b11_001110; //near -pi/4
     repeat(28) @(posedge clk);
     in=8'b11_001110;
     repeat(2) @(posedge clk);
